ec6eedc5ed19af9ef441607d8df6c3a5487795c6
Mohit Marwari <mmarwadi@gmail.com>
2016-01-15 17:24:48 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,6 +2,7 @@
 	if(!empty($_POST['details'])){
 		$new_task=$_POST['details'];
 		$connection=mysql_connect('localhost','root','')or die('Connection Error');
+		mysql_query("CREATE DATABASE IF NOT EXISTS db");
 		$db=mysql_select_db('db',$connection) or die('Cannot Select Database');
 		mysql_query('CREATE TABLE IF NOT EXISTS to_do (id int NOT NULL AUTO_INCREMENT, task varchar(255) NOT NULL,time TIMESTAMP NOT NULL DEFALUT CURRENT_TIMESTAMP,PRIMARY KEY(id) )');
 		$done=mysql_query("INSERT INTO to_do(task) VALUES ('$new_task')") or die ("Not Inserted");
