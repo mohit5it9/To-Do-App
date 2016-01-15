d74f75f44f2413bece7a71a1e1a04058b6537b7e
Mohit Marwari <mmarwadi@gmail.com>
2016-01-15 16:51:14 +0530

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,21 @@
+<?php
+	$uri=$_SERVER['REQUEST_URI'];
+	$url=$uri;
+	$uri=explode('?',$uri);
+	$keyword=$uri[1];
+	$keyword=explode('=',$keyword);
+	$delete=$keyword[0];
+	$delete=str_replace('+', ' ', $delete);
+	$connection=mysql_connect('localhost','root','')or die('Connection Error');
+	$db=mysql_select_db('db',$connection) or die('Cannot Select Database');
+	mysql_query('CREATE TABLE IF NOT EXISTS to_do (id int NOT NULL AUTO_INCREMENT, task varchar(255) NOT NULL,time TIMESTAMP NOT NULL DEFALUT CURRENT_TIMESTAMP,PRIMARY KEY(id) )');
+	$done=mysql_query("DELETE from to_do where task ='$delete'");
+	if($done==1){
+		header("location:index.php");
+	}
+	else{
+		echo "Error in Deleting";
+
+	}
+	mysql_close($connection);
+?>
