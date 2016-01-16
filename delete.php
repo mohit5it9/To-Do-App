834c1f8551522cb40655f4bbe2de3f5e49d46082
Mohit Marwari <mmarwadi@gmail.com>
2016-01-16 17:59:22 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,19 +1,8 @@
 <?php
-	$uri=$_SERVER['REQUEST_URI'];
-	$uri=explode('?',$uri);
-	$keyword=$uri[1];
-	$keyword=explode('=',$keyword);
-	$delete=$keyword[0];
-	$delete=str_replace('+', ' ', $delete);
+	$delete=$_POST['task_id'];
 	$connection=mysql_connect('localhost','root','')or die('Connection Error');
 	$db=mysql_select_db('db',$connection) or die('Cannot Select Database');
 	mysql_query('CREATE TABLE IF NOT EXISTS to_do (id int NOT NULL AUTO_INCREMENT, task varchar(255) NOT NULL,time TIMESTAMP NOT NULL DEFALUT CURRENT_TIMESTAMP,PRIMARY KEY(id) )');
-	$done=mysql_query("DELETE from to_do where task ='$delete'");
-	if($done==1){
-		header("location:index.php");
-	}
-	else{
-		echo "Error in Deleting";
-	}
+	mysql_query("DELETE from to_do where id ='$delete'");
 	mysql_close($connection);
 ?>
