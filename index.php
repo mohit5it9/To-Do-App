a57bd32748f9bd326a51cc06ecef60108b908498
Mohit Marwari <mmarwadi@gmail.com>
2016-01-16 17:57:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,48 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>
-			To-Do App
-		</title>
-	</head>
 	<body>
-		<table  border='1' style="border:2px solid grey;width:80%;margin-left:auto;margin-right:auto">
+		<table border='1'>
 			<tr>
-				<th>ID</th>
-				<th>Task</th>
-				<th>Time Stamp</th>
+				<th> ID </th>
+				<th> Task </th>
+				<th> Date </th>
 			</tr>
 			<?php 
-				$connection=mysql_connect('localhost','root','')or die('Connection Error');
-				$db=mysql_select_db('db',$connection) or die('Cannot Select Database');
-				mysql_query('CREATE TABLE IF NOT EXISTS to_do (id int NOT NULL AUTO_INCREMENT, task varchar(255) NOT NULL,time TIMESTAMP NOT NULL DEFALUT CURRENT_TIMESTAMP,PRIMARY KEY(id) )');
-				$query=mysql_query("select * from to_do ",$connection);
-			   while($row=mysql_fetch_assoc($query)) { ?>
-					<tr>
-						<td style="width:15%;text-align:center"><?php echo $row['id'];?></td>
-						<td style="width:25%;text-align:center"><?php echo $row['task'];?></td>
-						<td style="width:30%;"><?php echo $row['time'];?><form action="delete.php" method="get"><input name="<?php echo $row['task']?>" type="submit" value="Delete"></form></td>
+				$connection=mysql_connect("localhost","root","");
+				$db=mysql_select_db("db",$connection);
+				$table_contents=mysql_query("SELECT * from to_do order by time DESC");
+				while($row=mysql_fetch_assoc($table_contents)){?>
+					<tr class="row_details" >
+						<td> <?php echo $row['id'];?> </td>
+						<td> <?php echo $row['task'];?> </td>
+						<td> <?php echo $row['time'];?> </td>
+						<td> <button class='delete_button' id="<?php echo $row['id'];?>"> Delete </button> </td>
 					</tr>
-			<?php } ?>
-		</table>
-		<hr>
-		<form action="insert.php">
-  			New Task Description: <input type="text" id="details" name="details" placeholder='Enter your new To-Do'><br>
-  			<input type="submit" value="Submit">
-		</form>
+				<?php } ?>
+		</table><br>
+		Enter New Task: <input type="text" class="form_value"><input type="submit" class="form_submit" value="Add">
 	</body>
 </html>
+<script type="text/javascript" src="jquery-1.9.1.js"></script>
+<script type="text/javascript">
+	$('.delete_button').click(function(){
+		current_element=$(this);
+		task_to_be_deleted_id=$(this).attr('id');
+		$.post('delete.php',{task_id:task_to_be_deleted_id},function(){
+			current_element.parent().parent().fadeOut("slow",function(){
+				$(this).remove();
+			});
+		})
+	})
+	$('.form_submit').click(function(){
+		new_task_value=$('.form_value').val();
+		if(new_task_value!=''){
+			$.post('insert.php',{task:new_task_value},function(data){
+				$('.row_details').first().before(data).hide().fadeIn();
+			});
+		}
+		//return false; // if form submitted twice then do this
+		//x='<tr class="row_details"><td>'+x+'</td><td>'+new Date() +'</td></tr>';
+		//$('.row_details').last().after(x);
+	})
+</script>
