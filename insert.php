8e031ce1d42e99dedb31c8f5cd3251be976af6ac
Mohit Marwari <mmarwadi@gmail.com>
2016-01-16 17:58:52 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,16 +1,17 @@
 <?php
-	if(!empty($_POST['details'])){
-		$new_task=$_POST['details'];
+	if(!empty($_POST['task'])){
+		$new_task=$_POST['task'];
 		$connection=mysql_connect('localhost','root','')or die('Connection Error');
-		mysql_query("CREATE DATABASE IF NOT EXISTS db");
 		$db=mysql_select_db('db',$connection) or die('Cannot Select Database');
 		mysql_query('CREATE TABLE IF NOT EXISTS to_do (id int NOT NULL AUTO_INCREMENT, task varchar(255) NOT NULL,time TIMESTAMP NOT NULL DEFALUT CURRENT_TIMESTAMP,PRIMARY KEY(id) )');
-		$done=mysql_query("INSERT INTO to_do(task) VALUES ('$new_task')") or die ("Not Inserted");
-		if($done==1){
-			header('location:index.php');
+		mysql_query("INSERT INTO to_do(task) VALUES ('$new_task')") or die ("Not Inserted");
+		$query=mysql_query("Select * from to_do where task='$new_task'");
+		while($row=mysql_fetch_assoc($query)){
+			$new_task_details=$row['task'];
+			$new_task_time=$row['time'];
+			$new_task_id=$row['id'];
 		}
-		else{
-			echo 'Error';
-		}
+		mysql_close();
+		echo '<tr class="row_details"><td>'.$new_task_id.'</td><td>'.$new_task_details.'</td><td>'.$new_task_time.'</td><td><button class="delete_button" id='.$new_task_id.'>Delete</button></td></tr>';
 	}
 ?>
